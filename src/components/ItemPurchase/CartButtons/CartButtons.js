883870206626e27794b22b92f1340906ac3041bb
Jakeh Clark <jakehclarkcreates@gmail.com>
2018-11-01 11:09:08 -0500

    minor update

--- src/components/ItemPurchase/CartButtons/CartButtons.js
+++ src/components/ItemPurchase/CartButtons/CartButtons.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import displaylogic from './CartonButtons_DisplayLogic'
 
-const styles = theme => ({
-    root: {
-    },
-    cartButtons: {
-        padding: '2px',
-    }
-});
 
 class CartButtons extends Component {
 
@@ -22,7 +13,4 @@ class CartButtons extends Component {
     }
 }
 
-CartButtons.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-export default withStyles(styles)(CartButtons);
+export default CartButtons;
--- src/components/ItemPurchase/CartButtons/CartonButtons_DisplayLogic.test.js
+++ src/components/ItemPurchase/CartButtons/CartonButtons_DisplayLogic.test.js
@@ -1,5 +1,14 @@
-import * as funcs from './CartonButtons_DisplayLogic'
+import displaylogic from './CartonButtons_DisplayLogic'
 
 test('check if purchaseChannelCode is working', () => {
-    expect(funcs.whichTodisplay(0)).toBe(1);
+    expect(displaylogic(0)).toBe(
+        <Grid container item xs={12} item xs={12} style={{display:'flex'}} spacing={8}>
+                <Grid item xs={6}>
+                    <Button variant="contained" color='primary' fullWidth>PICK UP IN STORE</Button>
+                </Grid>
+                <Grid item xs={6}>
+                    <Button variant="contained" color='primary' fullWidth>ADD TO CART</Button>
+                </Grid>
+            </Grid>
+    );
   });
